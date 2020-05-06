a4ae9c919bf87400f946f2798162a7faba672f5b
Tarik <muradovt111@gmail.com>
2020-05-06 21:28:45 +0300

    Refactoring some MyPosts.js posts by adding an object with keys and properties into another variable. And after that in div Post I insert this data from variable.

--- src/components/Dialogues/Dialogues.js
+++ src/components/Dialogues/Dialogues.js
@@ -14,19 +14,36 @@ const Message = (props) => {
     return <div className={classes.messages}>{props.text}</div>
 }
 
-const Dialogues = () => {
+const Dialogues = (props) => {
+
+    let dialoguesData = [
+        {id: 1, name: "John"},
+        {id: 2, name: "Adam"},
+        {id: 3, name: "John"},
+        {id: 4, name: "John"},
+    ];
+
+    let messagesData = [
+        {id: 1, message: "Heya"},
+        {id: 2, message: "How's it going?"},
+        {id: 3, message: "Awesome! Better than you as usual"},
+        {id: 4, message: "Hey dickhead!"},
+    ];
+
     return (
         <div className={classes.dialogues}>
             <div className={classes.dialoguesItems}>
-                <ConversationItem name={'John'} id={'1'}/>
-                <ConversationItem name={'John'} id={'2'}/>
-                <ConversationItem name={'Mary'} id={'3'}/>
-                <ConversationItem name={'Steve'} id={'4'}/>
+                <ConversationItem name={dialoguesData[0].name} id={dialoguesData[0].id}/>
+                <ConversationItem name={dialoguesData[1].name} id={dialoguesData[1].id}/>
+                <ConversationItem name={dialoguesData[2].name} id={dialoguesData[2].id}/>
+                <ConversationItem name={dialoguesData[3].name} id={dialoguesData[3].id}/>
+
             </div>
             <div className={classes.messages}>
-                <Message text={'Heya!'} />
-                <Message text={"How's it going!"} />
-                <Message text={'Hey dickhead!'} />
+                <Message text={messagesData[0].message}/>
+                <Message text={messagesData[1].message}/>
+                <Message text={messagesData[2].message}/>
+                <Message text={messagesData[3].message}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -4,6 +4,14 @@ import classes from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postsData = [
+        {id: 1, post: "Heya! how's it going ?", likesCount: 2},
+        {id: 2, post: "Fine! how about you bro?", likesCount: 23},
+        {id: 3, post: "What the hell are you doing here?", likesCount: 228},
+        {id: 4, post: "Hey dickhead!", likesCount: 322},
+    ];
+
     return (
         <div className={classes.postsBlock}>
             <h2>My posts</h2>
@@ -17,9 +25,10 @@ const MyPosts = () => {
 
             </div>
             <div className={classes.posts}>
-                <Post message={"heya! how's it going ?"} likesCount={'2'}/>
-                <Post message={"Fine! how about you bro?"} likesCount={'6'}/>
-                <Post message={"What the hell are you doing here?"} likesCount={'22'}/>
+                <Post message={postsData[0].post} likesCount={postsData[0].likesCount}/>
+                <Post message={postsData[1].post} likesCount={postsData[1].likesCount}/>
+                <Post message={postsData[2].post} likesCount={postsData[2].likesCount}/>
+                <Post message={postsData[3].post} likesCount={postsData[3].likesCount}/>
             </div>
 
         </div>
