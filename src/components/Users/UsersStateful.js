2648e1b83299b5ec03f312ac9c328a4ca2e5ca4f
Tarik <muradovt111@gmail.com>
2020-05-29 21:41:30 +0300

    Refactoring UsersStateful.js and Users

--- src/components/Users/UsersStateful.js
+++ src/components/Users/UsersStateful.js
@@ -1,7 +1,38 @@
 import React from "react";
-import { connect } from "react-redux";
-import UsersStateless from './UsersStateless'
-import { followActionCreator, setUsersActionCreator, unFollowActionCreator, setCurrentPageActionCreator, setTotalUsersCountActionCreator } from "../../redux/users-reducer";
+import {connect} from "react-redux";
+import {
+    followActionCreator,
+    setUsersActionCreator,
+    unFollowActionCreator,
+    setCurrentPageActionCreator,
+    setTotalUsersCountActionCreator
+} from "../../redux/users-reducer";
+import * as axios from "axios"
+import Users from './Users'
+
+class UsersStateless extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage} &count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+
+    onPageChanged = (pageNumber) => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage} &count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setCurrentPage(pageNumber)
+            })
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount} pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChanged={this.onPageChanged} usersData={this.props.usersData} unFollow={this.props.unFollow}
+                      follow={this.props.follow}/>
+    }
+}
 
 let mapStateToProps = (state) => {
 
