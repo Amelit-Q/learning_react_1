a63b09a94f0979f4804ddd8481ce670a022e2cc9
Tarik <muradovt111@gmail.com>
2020-06-03 21:21:21 +0300

    Added logic for follow unfollow requests to the API

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,15 @@ import "./Header.module.css"
 import classes from './Header.module.css'
 import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={classes.header}>
             <img
                 src={'https://i.pinimg.com/originals/a4/e0/8c/a4e08c9d154ad706a058a7f7e915327d.jpg'} />
 
             <div className={classes.loginBlock}>
-                <NavLink to={'/login'}>Login</NavLink>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+
             </div>
         </header>
     )
